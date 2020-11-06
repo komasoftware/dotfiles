7b80a056fc2ce127a39e134706973dd8601d79d4
komasoftware <koen@koma.be>
2020-11-06 15:27:00 +0100

    initial personalisation for xubuntu

---
+++ google-cloud-sdk/completion.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/koen/google-cloud-sdk/path.zsh.inc' ]; then . '/home/koen/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/koen/google-cloud-sdk/completion.zsh.inc' ]; then . '/home/koen/google-cloud-sdk/completion.zsh.inc'; fi
